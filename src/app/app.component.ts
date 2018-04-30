de5f18da8bd1946bef297399d43d184cf7b92e99
Bobby Martin and kelsey guerra <bobby@>
2018-04-30 14:34:09 -0700

    Add initial files for Tuesdays class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { Task } from './models/task.model';
 
 @Component({
-  selector: 'app-root',
+  selector: 'Super-Cool-Selector',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+  currentFocus: string = 'Angular Homework';
+  currentTime = new Date();
+  month: number = this.currentTime.getMonth() + 1;
+  day: number = this.currentTime.getDate();
+  year: number = this.currentTime.getFullYear();
+  tasks: Task[] = [
+    new Task('Finish weekend Angular homework for Epicodus course'),
+    new Task('Begin brainstorming possible JavaScript group projects'),
+    new Task('Add README file to last few Angular repos on GitHub'),
+    new Task('Do the dishes'),
+    new Task('Think of a site or application to recreate for the two week code review')
+  ];
 }
