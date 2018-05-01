01d846a6678977e7f58e4c8669c01c963b925780
Bobby Martin and Isaac Shoman <isaac@>
2018-05-01 11:05:47 -0700

    Add edit form for clicked task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,10 +14,33 @@ export class AppComponent {
   day: number = this.currentTime.getDate();
   year: number = this.currentTime.getFullYear();
   tasks: Task[] = [
-    new Task('Finish weekend Angular homework for Epicodus course'),
-    new Task('Begin brainstorming possible JavaScript group projects'),
-    new Task('Add README file to last few Angular repos on GitHub'),
-    new Task('Do the dishes'),
-    new Task('Think of a site or application to recreate for the two week code review')
+    new Task('Finish weekend Angular homework for Epicodus course', 4),
+    new Task('Begin brainstorming possible JavaScript group projects', 3),
+    new Task('Add README file to last few Angular repos on GitHub', 5),
+    new Task('Do the dishes', 1),
+    new Task('Think of a site or application to recreate for the two week code review', 2)
   ];
+
+  selectedTask = null;
+
+  editTask(clickedTask) {
+    this.selectedTask = clickedTask;
+  }
+
+  priorityColor(currentTask) {
+    if(currentTask.priority === 5) {
+      return "bg-danger";
+    } else if (currentTask.priority === 4) {
+      return "bg-warning";
+    } else if (currentTask.priority === 3) {
+      return "bg-info";
+    } else if (currentTask.priority === 2) {
+      return "bg-secondary";
+    } else {
+      return "bg-success";
+    }
+  }
+  finishEditing() {
+    this.selectedTask = null;
+  }
 }
